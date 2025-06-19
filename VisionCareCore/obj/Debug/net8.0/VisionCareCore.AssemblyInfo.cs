c9962b25f820580a04c88ee33f623dafb3c6b205
BrunoPa20 <u201914974@upc.edu.pe>
2025-06-18 23:34:27 -0500

    cambios

--- VisionCareCore/obj/Debug/net8.0/VisionCareCore.AssemblyInfo.cs
+++ VisionCareCore/obj/Debug/net8.0/VisionCareCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VisionCareCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68373a4bbaafc07cb4b689e724664f1d17be27ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+307620dfc092c79614a2b7698e46f6ab0d3aa030")]
 [assembly: System.Reflection.AssemblyProductAttribute("VisionCareCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VisionCareCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
